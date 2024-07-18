dcd8c36a5289e158a17d49ad482a31d049ac8441
Zarko <stojanov1987@gmail.com>
2024-07-18 20:59:45 +0200

    * initial commit * axios * done

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import {Movies} from "./Components/Movies";
+
+const App = () => {
+    return (
+        <>
+            <Movies/>
+        </>
+    );
+}
+
+export default App;
--- src/Components/Movies.jsx
+++ src/Components/Movies.jsx
@@ -1,5 +1,25 @@
+import {useState} from "react";
+import axios from "axios";
 
 
- export const Movies = () => {
-    return<></>
+export const Movies = () => {
+
+    const [movieName, setMovieName] = useState()
+
+    const searchMovieByTitle = () => {
+
+        axios.get(`${process.env.REACT_APP_OMDBAPI_URL}?t=${movieName}&apikey=${process.env.REACT_APP_OMDBAPI_KEY}`)
+            .then(response => console.log(response.data))
+            .catch(error => console.log(error));
+    }
+
+    return <>
+
+        <form>
+            <input type='text' onInput={e => setMovieName(e.currentTarget.value)}/>
+            <button onClick={searchMovieByTitle} type='button'>Search Movie</button>
+        </form>
+
+    </>
 }
+
